18848c61afbf69a4cb0e85ea58e1ed3f3d2ffb69
Elyes Voisin <elyes.voisin@edu.devinci.fr>
2021-10-28 20:08:45 +0200

    :lipstick: ADD navbar and social

--- script/script.js
+++ script/script.js
@@ -1,18 +1,9 @@
 document.onreadystatechange = () => {
-    if (document.readyState === 'complete') {
-        let target = document.getElementsByClassName("loading")[0]
-        let fadeEffect = setInterval(function () {
-            if (!target.style.opacity) {
-                target.style.opacity = 1;
-            }
-            if (target.style.opacity > 0) {
-                target.style.opacity -= 0.1;
-            } else {
-                clearInterval(fadeEffect);
-            }
-        }, 10);
-    }
-};
+    const target = document.getElementsByClassName("loading")[0]
+
+    target.style.opacity = '0'
+    target.addEventListener('transitionend', () => target.remove());
+}
 
 
 
