af228d5ac2ff9c3f03dcc53fcb9dd57fa2c91cde
Elyes <elyesvoisin52@gmail.com>
2021-10-30 23:28:53 +0200

    :dizzy: ADD alternative loading disapear

--- script/script.js
+++ script/script.js
@@ -1,13 +1,16 @@
-document.onreadystatechange = () => {
+//document.onreadystatechange = () => {
+//    const target = document.getElementsByClassName("loading")[0]
+//
+//    target.style.opacity = '0'
+//    target.style.zIndex = '0'
+//    target.addEventListener('transitionend', () => target.remove());
+//}
+
+setTimeout(function () {
     const target = document.getElementsByClassName("loading")[0]
 
     target.style.opacity = '0'
     target.style.zIndex = '0'
     target.addEventListener('transitionend', () => target.remove());
-}
-
-
-
-
-
+}, 1000);
 
